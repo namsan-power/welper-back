20635228a36b32894b812efa803d511b10142874
hyunwoo <dlagusdn0218@gmail.com>
2025-11-04 04:27:12 +0900

    Init : 프로젝트 초기 세팅

---
+++ src/main/java/com/example/welperback/WelperBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.welperback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelperBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelperBackApplication.class, args);
+    }
+
+}
