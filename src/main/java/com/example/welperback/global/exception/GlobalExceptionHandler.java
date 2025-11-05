0f2db8748ddfdb87dc03140408dd58478f2705f3
hyunwoo <dlagusdn0218@gmail.com>
2025-11-06 03:06:37 +0900

    Fix : Sagger 전역처리 어노테이션 주석

--- src/main/java/com/example/welperback/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/welperback/global/exception/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+//@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
